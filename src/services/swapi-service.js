ccf52b11078f0bad22bc6ddd559e4a51252a7600
Kamshat Kaliyeva <k.kaliyeva@maxpay.kz>
2023-11-23 10:12:29 +0600

    I have supplement API and added methods for thansforming data from server

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -3,24 +3,26 @@ export default class SwapiService {
     _apiBase = 'https://swapi.co/api';
 
     async getResource(url) {
-        const res = await fetch(`${this._apiBase}${url}`);
+        const res = await fetch(this._apiBase + url);
+        if(!res.ok)
+            throw new Error(`Could not fetch ${url}, received ${res.status}`);
 
-        if(!res.ok) throw new Error(`Could not fetch ${url}, received ${res.status}`);
-        return  await res.json();
+        return await res.json();
     }
 
     async getAllPeople() {
         const res = await this.getResource(`/people/`);
-        return res.results
+        return res.results.map(this._transformPerson);
     }
 
-    getPerson(id) {
-        return this.getResource(`/people/${id}`)
+    async getPerson(id) {
+        const person = await this.getResource(`/people/${id}/`);
+        return this._transformPerson(person);
     }
 
     async getAllPlanets() {
         const res = await this.getResource(`/planets/`);
-        return res.results
+        return res.results.map(this._transformPlanet);
     }
 
     getPlanet(id) {
@@ -29,10 +31,38 @@ export default class SwapiService {
 
     async getAllStarships() {
         const res = await this.getResource(`/starships/`);
-        return res.results
+        return res.results.map(this._transformStarship);
+    }
+    async getStarship(id) {
+        const starship = await this.getResource(`/starships/${id}/`);
+        return this._transformStarship(starship)
     }
 
-    getStarship(id) {
-        return this.getResource(`/starships/${id}`)
+    _extractId(item) {
+        const idRegExp = /\/([0-9]*)\/$/;
+        return item.url.match(idRegExp)[1];
+    }
+    _transformStarship = (starship) => {
+        return {
+            id: this._extractId(starship),
+            name: starship.name,
+            model: starship.model,
+            manufacturer: starship.manufacturer,
+            costInCredits: starship.cost_in_credits,
+            length: starship.length,
+            crew: starship.crew,
+            passengers: starship.passengers,
+            cargoCapacity: starship.cargo_capacity
+        }
+    };
+
+    _transformPerson = (person) => {
+        return {
+            id: this._extractId(person),
+            name: person.name,
+            gender: person.gender,
+            birthYear: person.birth_year,
+            eyeColor: person.eye_color
+        }
     }
 }
