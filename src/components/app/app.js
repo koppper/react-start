b429b1fc42384d5a9c63d75f9438690ffb8b66e0
Kamshat Kaliyeva <k.kaliyeva@maxpay.kz>
2023-11-23 10:03:35 +0600

    Application markup was created

---
+++ src/components/app/app.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import Header from '../header';
+import RandomPlanet from '../random-planet';
+import ItemList from '../item-list';
+import PersonDetails from '../person-details';
+
+import './app.css';
+
+const App = () => {
+    return (
+        // Разметка приложения с использованием Bootstrap-классов
+        <div className='container-fluid'>
+            <div className="row">
+                <div className="col-12">
+                    <Header/>
+                    <RandomPlanet />
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-6">
+                    <ItemList />
+                </div>
+                <div className="col-md-6">
+                    <PersonDetails />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,51 +1,6 @@
-// Создание класса SwapiService для взаимодействия с API Star Wars (SWAPI)
-class SwapiService {
-    _apiBase = 'https://swapi.co/api';  // Базовый URL API
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-    // Метод getResource для выполнения HTTP-запроса к API по указанному URL
-    async getResource(url) {
-        const res = await fetch(`${this._apiBase}${url}`);  // Использование fetch для выполнения запроса
-        if (!res.ok) throw new Error(`Could not fetch ${url}, received ${res.status}`);  // Обработка ошибок HTTP
-        return await res.json();  // Возврат результата в формате JSON
-    }
-
-    // Метод getAllPeople для получения всех персонажей из API
-    async getAllPeople() {
-        const res = await this.getResource(`/people/`);  // Вызов метода getResource с конкретным URL
-        return res.results;  // Возврат массива персонажей
-    }
-
-    // Метод getPerson для получения информации о конкретном персонаже по его ID
-    getPerson(id) {
-        return this.getResource(`/people/${id}`);  // Вызов метода getResource с конкретным URL для персонажа
-    }
-
-    // Метод getAllPlanets для получения всех планет из API
-    async getAllPlanets() {
-        const res = await this.getResource(`/planets/`);  // Вызов метода getResource с конкретным URL
-        return res.results;  // Возврат массива планет
-    }
-
-    // Метод getPlanet для получения информации о конкретной планете по её ID
-    getPlanet(id) {
-        return this.getResource(`/planet/${id}`);  // Вызов метода getResource с конкретным URL для планеты
-    }
-
-    // Метод getAllStarships для получения всех звездолётов из API
-    async getAllStarships() {
-        const res = await this.getResource(`/starships/`);  // Вызов метода getResource с конкретным URL
-        return res.results;  // Возврат массива звездолётов
-    }
-
-    // Метод getStarship для получения информации о конкретном звездолёте по его ID
-    getStarship(id) {
-        return this.getResource(`/starships/${id}`);  // Вызов метода getResource с конкретным URL для звездолёта
-    }
-}
-
-// Создание экземпляра класса SwapiService
-const swapi = new SwapiService();
-// Вызов метода getPerson с параметром ID (например, 3) и вывод имени персонажа в консоль
-swapi.getPerson(3).then(p => {
-    console.log(p.name);
-});
+ReactDOM.render(<App/>,
+    document.getElementById('root'));
